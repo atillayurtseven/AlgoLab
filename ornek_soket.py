d666190f52dfe5f6a0c584752545ef92ed647ce2
Atilla Yurtseven <42356316+atillayurtseven@users.noreply.github.com>
2023-08-04 17:26:21 +0300

    Upgrade

--- ornek_soket.py
+++ ornek_soket.py
@@ -1,20 +1,34 @@
-from deniz_yatirim import AlgoLab
+from algolab import AlgoLab
 from algolab_socket import AlgoLabSocket
+import json
+import time
 
-# KULLANICI BİLGİSİ
-
+# USER INFO
 API_KEY = "API_KEY"
 USERNAME = "TC_KİMLİK_NO"
 PASSWORD = "DENİZBANK_ŞİFRENİZ"
 
-def process_msg(msg):
-    try:
-        t = msg["Type"]
-        content = msg["Content"]
-        print(content)
-    except:
-        pass
-
 if __name__ == "__main__":
+    symbol = "GARAN"
+    period = "60" # 60 dk
     algo = AlgoLab(api_key=API_KEY, username=USERNAME, password=PASSWORD, auto_login=True)
-    soket = AlgoLabSocket(algo.api_key, algo.hash, "T", callback=process_msg)
+    soket = AlgoLabSocket(algo.api_key, algo.hash, "T")
+    soket.connect()
+    while not soket.connected:
+        time.sleep(0.05)
+
+    data = {"Type": "T", "Symbols": ["GARAN", "TSKB"]}
+    soket.send(data)
+
+    i = 0
+    while soket.connected:
+        data = soket.recv()
+        i += 1
+        if data:
+            try:
+                msg = json.loads(data)
+                print(msg)
+            except:
+                print("error 1")
+                soket.close()
+                break
