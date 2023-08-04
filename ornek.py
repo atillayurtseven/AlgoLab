de1cf777f40870f3caa689f66022a256dcd75288
Atilla Yurtseven <42356316+atillayurtseven@users.noreply.github.com>
2023-08-04 17:09:32 +0300

    Upgrade

--- ornek.py
+++ ornek.py
@@ -19,24 +19,25 @@ if __name__ == "__main__":
             # GARAN hissesinin 60 dk'lık geçmişini alıyoruz. DİKKAT: Seans içindeyseniz son bar henüz kapanmamıştır.
             candle = d.GetCandleData(symbol, period)
             if candle:
+                print(candle)
                 try:
-                    succ = candle["Success"]
+                    succ = candle["success"]
                     if succ:
                         ohlc = []
-                        content = candle["Content"]
+                        content = candle["content"]
                         for i in range(len(content)):
-                            d = content[i]["Date"]
+                            d = content[i]["date"]
                             try:
                                 dt = datetime.datetime.strptime(d, "%Y-%m-%dT%H:%M:%S").strftime("%Y-%m-%d %H:%M:%S")
                             except:
                                 dt = datetime.datetime.strptime(d, "%Y-%m-%dT%H:%M:%S%z").strftime("%Y-%m-%d %H:%M:%S")
-                            o = content[i]["Open"]
-                            h = content[i]["High"]
-                            l = content[i]["Low"]
-                            c = content[i]["Close"]
+                            o = content[i]["open"]
+                            h = content[i]["high"]
+                            l = content[i]["low"]
+                            c = content[i]["close"]
                             ohlc.append([dt, o, h, l, c])
                         # oluşturduğumuz listi pandas dataframe'e aktarıyoruz
-                        df = pd.DataFrame(columns=["Date", "Open", "High", "Low", "Close"], data=np.array(ohlc))
+                        df = pd.DataFrame(columns=["date", "open", "high", "low", "close"], data=np.array(ohlc))
                         print(df.tail())
                 except Exception as e:
                     print(f"Hata oluştu: {e}")
